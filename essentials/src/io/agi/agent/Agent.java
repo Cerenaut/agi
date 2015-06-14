dd02dee04f12de58545004b68c663e861508e853
Gideon Kowadlo <gideon@outware.com.au>
2015-06-14 16:52:59 +1000

    Intermediate check in: Swagger-UI js client, configured to show the swagger spec being served at /api. However, not working.

---
+++ essentials/src/io/agi/agent/Agent.java
@@ -0,0 +1,15 @@
+package io.agi.agent;
+
+import org.eclipse.jetty.client.HttpClient;
+
+/**
+ * Created by gideon on 14/06/15.
+ */
+public class Agent {
+//
+//    HttpClient client = new HttpClient();
+//    client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
+//    client.setTimeout(10000); // 10 seconds timeout; if no server reply, the request expires
+//    client.start();
+
+}
--- essentials/src/io/agi/http/StaticServletServer.java
+++ essentials/src/io/agi/http/StaticServletServer.java
@@ -6,14 +6,18 @@
 
 package io.agi.http;
 
-import io.agi.coordinator.ServletContextHandlerControl;
-import io.agi.coordinator.ServletContextHandlerData;
 import io.agi.properties.AGIProperties;
+
+import java.awt.*;
 import java.util.ArrayList;
+
+import org.eclipse.jetty.http.MimeTypes;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+
+import javax.activation.MimeType;
 
 /**
  * A simple Web server based on Jetty.
@@ -26,23 +30,27 @@ public class StaticServletServer {
 
     public static final String SERVLET_CONTEXT_PATHSPEC = "/"; // xLabs Web Services
     public static final String STATIC_CONTEXT_PATHSPEC = "/"; // xLabs Web Services
-    
+
     public static final String PROPERTY_PORT = "port"; 
     public static final int DEFAULT_PORT = 80;
 
     public static final String PROPERTY_SERVLET_CONTEXT = "servletContext";
     public static final String PROPERTY_STATIC_CONTEXT = "staticContext";
+    public static final String PROPERTY_APIDOCS_CONTEXT = "apiDocs";
     public static final String PROPERTY_STATIC_DIRECTORY = "staticDirectory";
 
     public static final String DEFAULT_SERVLET_CONTEXT = "/";
     public static final String DEFAULT_STATIC_CONTEXT = "/";
+    public static final String DEFAULT_APIDOCS_CONTEXT = "/api";
     public static final String DEFAULT_STATIC_DIRECTORY = "./www";
 
     org.eclipse.jetty.server.Server _s;
     int _port;
     String _servletContext;
     String _staticContext;
+    String _apiDocsContext;
     String _staticDirectory;
+
 
     private ContextHandlerCollection _contextHandlers = null;
 
@@ -50,12 +58,14 @@ public class StaticServletServer {
         _port = DEFAULT_PORT;
         _servletContext  = DEFAULT_SERVLET_CONTEXT;
         _staticContext   = DEFAULT_STATIC_CONTEXT;
+        _apiDocsContext  = DEFAULT_APIDOCS_CONTEXT;
         _staticDirectory = DEFAULT_STATIC_DIRECTORY;
         
         for( String file : propertiesFiles ) {
             _port = Integer.valueOf( AGIProperties.get( file, PROPERTY_PORT, String.valueOf( _port ) ) );
             _servletContext  = AGIProperties.get( file, PROPERTY_SERVLET_CONTEXT, _servletContext );
             _staticContext   = AGIProperties.get( file, PROPERTY_STATIC_CONTEXT, _staticContext );
+            _apiDocsContext  = AGIProperties.get( file, PROPERTY_APIDOCS_CONTEXT, _apiDocsContext );
             _staticDirectory = AGIProperties.get( file, PROPERTY_STATIC_DIRECTORY, _staticDirectory );
         }
     }
@@ -96,6 +106,22 @@ public class StaticServletServer {
             ch.setClassLoader( Thread.currentThread().getContextClassLoader() );
             ch.setHandler( rh );
             _contextHandlers.addHandler( ch );
+
+            // add file describing Swagger API Spec
+            ResourceHandler rh1 = new ResourceHandler();
+            rh1.setDirectoriesListed( true );
+            //rh1.setWelcomeFiles( new String[]{ "swagger.json" } );
+            rh1.setResourceBase( _staticDirectory );
+
+            MimeTypes mimeTypes=new MimeTypes();
+            mimeTypes.addMimeMapping("json","application/json");
+            rh1.setMimeTypes( mimeTypes );
+
+            ContextHandler ch1 = new ContextHandler();
+            ch1.setContextPath( _apiDocsContext );
+            ch1.setClassLoader( Thread.currentThread().getContextClassLoader() );
+            ch1.setHandler( rh1 );
+            _contextHandlers.addHandler( ch1 );
 
             // add servlet contexts:
             ServletContextHandler shc = new ServletContextHandler( _contextHandlers, _servletContext );
