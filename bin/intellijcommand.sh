85fd87133959949523ca80df2171a7c098d0f7af
Gideon Kowadlo <gideon@agi.io>
2015-10-11 19:06:45 +1100

    Remove unnecessary intellij folder, and temporary files from /bin

--- bin/intellijcommand.sh
+++
@@ -1,5 +0,0 @@
-$JAVA_HOME/bin/java -Dfile.encoding=UTF-8  \
--cp \
-/Users/gideon/.m2/repository/com/sun/jersey/jersey-core/1.18/jersey-core-1.18.jar:\
-$AGI_HOME/src/experimental-framework/core-modules/target/io-agi-agief-core-modules-1.1.0-jar-with-dependencies.jar \
-io.agi.ef.http.node.NodeMain localhost 8080 NodeName 8081 server.properties COORDINATOR
