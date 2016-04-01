7dd3a4fc6be1f70725005b8f34c703cf7f34f88a
Gideon Kowadlo <gideon@outware.com.au>
2016-04-01 14:28:43 +1100

    Make project runnable on command line - this involved including maven dependencies via command line (not through direct reference to .m2 file in intelliJ project), making it a fat jar, and modifying the run script.

--- algorithms/code/core/runImage/run1.sh
+++ algorithms/code/core/runImage/run1.sh
@@ -1,4 +1,2 @@
-export JAVA_HOME=/home/dave/workspace/agi.io/java/jdk1.8.0_60
-export PATH=${JAVA_HOME}/bin:${PATH}
 
-java  -cp "/home/dave/workspace/agi.io/agi/algorithms/code/core/out/artifacts/core_jar/org.json-2.0.jar:/home/dave/workspace/agi.io/agi/algorithms/code/core/out/artifacts/core_jar/postgresql-9.2-1002.jdbc4.jar:/home/dave/workspace/agi.io/agi/algorithms/code/core/out/artifacts/core_jar/core.jar" io.agi.ef.demo.LightDemo node.properties entities.json refs.json
+java -cp "/Users/gideon/Development/AI/ProAGI/agi/algorithms/code/core/runImage/:/Users/gideon/Development/AI/ProAGI/agi/algorithms/code/core/target/io-agi-core-1.0.0-jar-with-dependencies.jar" io.agi.framework.demo.mnist.MNISTDemo node.properties entities.json refs.json properties.json
