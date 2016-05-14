bc2f0bd2d9d35c59e70cea3e2f6fd1703e5bc899
Gideon Kowadlo <gideon@outware.com.au>
2016-05-14 13:16:14 +1000

    added script to start/stop ec2 instances

---
+++ bin/aws-ecs/aws-ec2-start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$1" == "-h" -o "$1" == "--help" ]; then
+  echo "Usage: `basename $0` InstanceId X(1=START/0=STOP)"
+  echo "All arguments are optional."
+  exit 0
+fi
+
+# parameters
+start=${1:-1}
+instanceId=${2:-i-cf55394d}
+
+
+if [ "$start" == 1 ]; then
+	aws ec2 start-instances --instance-ids $instanceId
+else
+	aws ec2 stop-instances --instance-ids $instanceId
+fi
