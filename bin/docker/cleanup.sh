6ff875e8b57a183011ae030952f3a2ffa8924abb
Gideon Kowadlo <gideon@outware.com.au>
2016-09-27 23:01:42 +1000

    Convenience script for cleaning up docker imges

---
+++ bin/docker/cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# remove exited containers:
+docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+    
+# remove unused images:
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
