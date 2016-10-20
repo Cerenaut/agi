625f650d36ee9d575981151aec682f9598b1e671
davidra <divad.nosnilwar@gmail.com>
2016-10-20 15:19:30 +1100

    on phone

--- bin/variables-jenkins.sh
+++ bin/variables-jenkins.sh
@@ -12,7 +12,8 @@
 export AGI_HOME=$WORKSPACE/agi
 
 # RUN Home
-export AGI_RUN_HOME=$WORKSPACE/experiment-definitions/mnist-gng-v1
+#export AGI_RUN_HOME=$WORKSPACE/experiment-definitions/mnist-gng-v1
+export AGI_RUN_HOME=$WORKSPACE/experiment-definitions/mnist-autoencoder-v2
 
 # DATA Home (set to the home of the data for the actual experiment - i.e. in the environment where they'll be running)
 # export AGI_DATA_HOME=~/Development/ProjectAGI/AGIEF/datasets/MNIST      # when running locally
@@ -21,3 +22,4 @@ export AGI_DATA_HOME=/root/dev/data
 # Database
 export DB_PORT=5432
 # export DB_HOST=localhost		# IMPORTANT!!!  DO NOT DEFINE THIS VARIABLE.   It will be defined by scripts at runtime, and we don't want it to get defined by sourcing this file
+
