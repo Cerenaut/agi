a4404900882cf55e6749ee0edd731ffae598afa2
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-12-08 12:23:22 +1100

    Define runTime param in ExperimentEntityConfig

--- code/core/src/main/java/io/agi/framework/entities/ExperimentEntityConfig.java
+++ code/core/src/main/java/io/agi/framework/entities/ExperimentEntityConfig.java
@@ -34,6 +34,7 @@ public class ExperimentEntityConfig extends EntityConfig {
     public String terminationEntityName;
     public String terminationConfigPath;
     public int terminationAge = -1; // if negative, then never terminates unless via termination condition.
+    public long runTime = 0;
 
     public String reportingEntityName;
     public String reportingEntityConfigPath;
